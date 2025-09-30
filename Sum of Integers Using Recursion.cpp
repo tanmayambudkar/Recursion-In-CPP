bf6274562de73d5ac55db0b398fba1766b8d6236
Tanmay Ambudkar <tanmayambudkar@gmail.com>
2025-09-30 23:06:04 +0530

    Add files via upload

---
+++ Sum of Integers Using Recursion.cpp
@@ -0,0 +1,18 @@
+//Tanmay Ambudkar
+#include<iostream>
+using namespace std;
+int add(int n){
+    if(n<=1){ 
+        return 1;
+    }else{
+        return n+add(n-1); 
+    }
+}
+
+int main(){
+    int a,n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    a=add(n); 
+    cout<<a;
+}

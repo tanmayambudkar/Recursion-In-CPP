8973abd6b1cb0be12a19dbd21e5fe80388409cd6
Tanmay Ambudkar <tanmayambudkar@gmail.com>
2025-09-30 23:09:30 +0530

    Add files via upload

---
+++ Reverse String Using Recursion.cpp
@@ -0,0 +1,17 @@
+//Tanmay Ambudkar
+#include<iostream>
+using namespace std;
+void rev(char*str){
+   if (*str)
+   {
+       rev(str+1);
+       cout<<("%c", *str);
+   }
+}
+ 
+int main(){
+   char ch[50];
+   cout<<"Enter a string: ";
+   cin>>ch;
+   rev(ch);
+}
